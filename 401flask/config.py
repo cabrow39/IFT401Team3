e8a0608d6fe637440d2f4ef0690b694c3ef6c802
cabrow39 <cabrow39@asu.edu>
2024-03-12 09:24:41 -0700

    Flask with Database

---
+++ 401flask/config.py
@@ -0,0 +1,30 @@
+import os
+from datetime import timedelta
+
+# Determine the folder of the top-level directory of this project
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    WTF_CSRF_ENABLED = False
+    FLASK_ENV = 'development'
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.getenv('SECRET_KEY', default='BAD_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
+                                        default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REMEMBER_COOKIE_DURATION = timedelta(days=14)
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI',
+                                        default=f"sqlite:///{os.path.join(BASEDIR, 'instance', 'test.db')}")
+class ProductionConfig(Config):
+    FLASK_ENV = 'production'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
---
+++ 401flask/project/models.py
@@ -0,0 +1,66 @@
+from project import database
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column
+from werkzeug.security import generate_password_hash, check_password_hash
+import flask_login
+
+class User(flask_login.UserMixin, database.Model):  # UPDATED!
+    """
+    Class that represents a user of the application
+
+    The following attributes of a user are stored in this table:
+        * email - email address of the user
+        * hashed password - hashed password (using werkzeug.security)
+
+    REMEMBER: Never store the plaintext password in a database!
+    """
+    __tablename__ = 'users'
+
+    id = mapped_column(Integer(), primary_key=True)
+    email = mapped_column(String(), unique=True)
+    password_hashed = mapped_column(String(128))
+
+    def __init__(self, email: str, password_plaintext: str):
+        self.email = email
+        self.password_hashed = self._generate_password_hash(password_plaintext)
+
+    def is_password_correct(self, password_plaintext: str):
+        return check_password_hash(self.password_hashed, password_plaintext)
+
+    @staticmethod
+    def _generate_password_hash(password_plaintext):
+        return generate_password_hash(password_plaintext)
+
+    def __repr__(self):
+        return f'<User: {self.email}>'
+
+class Stock(database.Model):
+    """
+    Class that represents a purchased stock in a portfolio.
+
+    The following attributes of a stock are stored in this table:
+        stock symbol (type: string)
+        number of shares (type: integer)
+        purchase price (type: integer)
+
+    Note: Due to a limitation in the data types supported by SQLite, the
+          purchase price is stored as an integer:
+              $24.10 -> 2410
+              $100.00 -> 10000
+              $87.65 -> 8765
+    """
+
+    __tablename__ = 'stocks'
+
+    id = mapped_column(Integer(), primary_key=True)
+    stock_symbol = mapped_column(String())
+    number_of_shares = mapped_column(Integer())
+    purchase_price = mapped_column(Integer())
+
+    def __init__(self, stock_symbol: str, number_of_shares: str, purchase_price: str):
+        self.stock_symbol = stock_symbol
+        self.number_of_shares = int(number_of_shares)
+        self.purchase_price = int(float(purchase_price) * 100)
+
+    def __repr__(self):
+        return f'{self.stock_symbol} - {self.number_of_shares} shares purchased at ${self.purchase_price / 100}'
